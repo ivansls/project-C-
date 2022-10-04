41ff2a6cf997e4763f0fdca083f2925c51874d33
Иван Салдайкин <vanyasls123@gmail.com>
2022-10-04 09:42:08 +0300

    99% done program

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,59 +6,74 @@ Console.WriteLine("Выберите номер программы которую
     "3 - Вывод делителей числа\r\n" +
     "4 - выход");
 bool isListening = true;
+
+static void Game()
+{
+    bool isListening1 = true;
+    Random rnd = new Random();
+    int value = rnd.Next(0, 100);
+    do
+    {
+        Console.WriteLine("Введите число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        if (number == value)
+        {
+            Console.WriteLine("Молодец, ты угадал число!");
+            isListening1 = false;
+        }
+        else if (number > value)
+        {
+            Console.WriteLine("Попробуй взять меньше");
+        }
+        else
+        {
+            Console.WriteLine("Попробуй взять больше");
+        }
+    } while (isListening1 == true);
+}
+
+static void Table()
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        for (int j = 1; j <= 10; j++)
+        {
+            Console.Write((i * j) + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+static void Divider()
+{
+    Console.WriteLine("Введите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    string c = " ";
+    for (int i = 1; i <= number; i++)
+    {
+        if (number % i == 0)
+        {
+            c += i + " ";
+        }
+    }
+    Console.WriteLine(c);
+}
+
 do
 {
     Console.WriteLine("Введите номер команды:");
     int answer = Convert.ToInt32(Console.ReadLine());
     if (answer == 1)
     {
-        bool isListening1 = true;
-        Random rnd = new Random();
-        int value = rnd.Next(0, 100);
-        do
-        {
-            Console.WriteLine("Введите число: ");
-            int number = Convert.ToInt32(Console.ReadLine());
-            if (number == value)
-            {
-                Console.WriteLine("Молодец, ты угадал число!");
-                isListening1 = false;
-            }
-            else if (number > value)
-            {
-                Console.WriteLine("Попробуй взять меньше");
-            }
-            else
-            {
-                Console.WriteLine("Попробуй взять больше");
-            }
-        } while (isListening1 == true);
-
+        Game();
     }
     else if (answer == 2) 
     {
-        for (int i = 1; i <= 10; i++)
-        {
-            for (int j = 1; j <= 10; j++)
-            {
-                Console.Write((i * j) + "\t");
-            }
-            Console.WriteLine();
-        }
+        Table();
     }
     else if (answer == 3)
     {
-        Console.WriteLine("Введите число: ");
-        int number = Convert.ToInt32(Console.ReadLine());
-        string c = " ";
-        for (int i = 1; i <= number; i++)
-        {
-            if (number % i == 0)
-            {
-                c += i + " ";   
-            }
-        }
-        Console.WriteLine(c);
+        Divider();
     }
     else if (answer == 4)
     {
